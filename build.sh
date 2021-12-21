81f218e9d12eae4db33fe716c30dcdaafae277b2
Tomas Gonzalez Aragon <tomas.gonzalez.aragon@intel.com>
2021-12-21 15:12:52 -0800

    disable debug; use realpath in build scripts

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cmake -S . -B build \
 -DCMAKE_CXX_COMPILER=g++ \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=install \
--DCMAKE_PREFIX_PATH="../tgonzale-hexl-fpga/install" \
+-DCMAKE_PREFIX_PATH="$(realpath ${PWD}/../tgonzale-hexl-fpga/install)" \
 -DHEXL_TESTING=OFF \
 -DHEXL_BENCHMARK=OFF \
 -DHEXL_EXPERIMENTAL=ON \
--- example/build.sh
+++ example/build.sh
@@ -6,7 +6,7 @@ cmake -S cmake -B build \
 -DCMAKE_C_COMPILER=gcc \
 -DCMAKE_CXX_COMPILER=g++ \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_PREFIX_PATH="../../install;../../../tgonzale-hexl-fpga/install" \
+-DCMAKE_PREFIX_PATH="$(realpath ${PWD}/../../install);$(realpath ${PWD}/../../../tgonzale-hexl-fpga/install)" \
 -DHEXL_FPGA=ON
 
 cmake --build build
