e16ec8c559645d7b75fcc6cb052339e11a616cd9
Tomas Gonzalez Aragon <tomas.gonzalez.aragon@intel.com>
2022-01-10 15:54:11 -0800

    fix experimental

--- hexl/experimental/seal/ckks-multiply.cpp
+++ hexl/experimental/seal/ckks-multiply.cpp
@@ -5,15 +5,6 @@
 #include "hexl/experimental/seal/ckks-multiply.hpp"
 #include "hexl/experimental/seal/ckks-multiply-cpu.hpp"
 
-#include <cstring>
-
-#include "hexl/eltwise/eltwise-add-mod.hpp"
-#include "hexl/eltwise/eltwise-mult-mod.hpp"
-#include "hexl/number-theory/number-theory.hpp"
-#include "hexl/util/aligned-allocator.hpp"
-#include "hexl/util/check.hpp"
-#include "util/cpu-features.hpp"
-
 namespace intel {
 namespace hexl {
 
